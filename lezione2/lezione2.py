bb81a60d266a776028d288d48c58ff8dadf1f0d4
arcman1510 <arcman1510@gmail.com>
2024-04-18 13:45:50 +0200

    Esercizio 2-8 riisolto

--- lezione2/lezione2.py
+++ lezione2/lezione2.py
@@ -50,16 +50,20 @@ print("")
 print("Albert Einstein once said, “A person who never made a mistake never tried anything new.” ")
 print("")
 
-#2-6. Famous Quote 2: Repeat Exercise 2-5, but this time, r
-#epresent the famous person’s name using a variable called famous_person. 
+#2-6. Famous Quote 2: Repeat Exercise 2-5, but this time, 
+#represent the famous person’s name using a variable called famous_person. 
 #Then compose your message and represent it with a new variable called message. Print your message. 
 
 famous_person: str = "Albert Einstein"
 message: str = ("once said, 'A person who never made a mistake never tried anything new.'")
 print(famous_person, message)
-
+print("")
 
 #2-8. File Extensions: 
 #Python has a removesuffix() method that works exactly like removeprefix(). 
 #Assign the value 'python_notes.txt' to a variable called filename. 
 #Then use the removesuffix() method to display the filename without the file extension, like some file browsers do.
+
+filename: str = 'python_notes.txt'
+print(filename)
+print(filename.removesuffix(".txt"))
