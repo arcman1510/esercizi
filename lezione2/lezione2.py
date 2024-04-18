cd1e3b5b9aa110ed8f9f960cc12dcb5d6ee53f53
arcman1510 <arcman1510@gmail.com>
2024-04-18 12:25:05 +0200

    Risolto primo esercizio

---
+++ lezione2/lezione2.py
@@ -0,0 +1,21 @@
+#Manuel Archer
+#18/04/2024
+
+print("Hello World")
+
+#2.3 Personal Message: Use a variable to represent a person's name,
+#
+#
+#
+
+
+# Questa variabile contiene il nome
+name: str = "Mario"
+print(f"Ciao {name}, etc....")
+
+# Questa variabile contiene il messaggio
+name: str = "Mario"
+message: str = (f"Ciao {name} , ti va di imparare un po di python insieme?")
+
+print(message)
+
