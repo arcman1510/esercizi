e3ba3cf6509d7660dda9d940f3c0bf3230d98577
arcman1510 <arcman1510@gmail.com>
2024-04-19 12:01:08 +0200

    Creati file esercizi4 e lezioni4

---
+++ lezione4/esercizi4.py
@@ -0,0 +1,6 @@
+#let's try to define a funciton named "subtract" ourselvese
+#it should take 2 parameters
+#inside the funciton, it should subtract the to
+#then, return the result
+#after you defined it, call the funciton with some arguments
+
