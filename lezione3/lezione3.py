18bd61fe07f192522b75833c62a018c53e1a3e94
arcman1510 <arcman1510@gmail.com>
2024-04-19 11:53:34 +0200

    Appunti

--- lezione3/lezione3.py
+++ lezione3/lezione3.py
@@ -6,10 +6,13 @@ print(lista)
 y = min(lista)
 print(lista)
 
+
+print("")
 #definiamo le variabile che useremo per calcolare l'area
 #scrivo la riga di stampa dove chiamo le variabili x ed y
 # il return mi serve per restituirmi le due variabili moltiplicate
 #   l'unico modo per "comunicare col mondo esterno"
+# è se eseguo il "print"
 # posso assegnare un'altra variabile out che equivale a questa molt.
 #
 def area(x:float,y:float) -> float:
@@ -18,10 +21,11 @@ def area(x:float,y:float) -> float:
     out = x * y
     print(f"l'area è {out}")
 
-""""
+#questo paragrafo mi stampa non solo nuove stringhe
+#ma avendo i numeri assegnati ai numeri
+#posso eseguire l'operazione
 print("bella")
 x,y = (2, 3)
 #print(area(x,y))
 out = area(x, y)
 print(out)
-"""
