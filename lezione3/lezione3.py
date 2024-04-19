7d9d6e99e9d451a605b8b47f7bbc7e5f6c9c7d78
arcman1510 <arcman1510@gmail.com>
2024-04-19 11:45:12 +0200

    Calcolare l'area x e y

--- lezione3/lezione3.py
+++ lezione3/lezione3.py
@@ -6,3 +6,22 @@ print(lista)
 y = min(lista)
 print(lista)
 
+#definiamo le variabile che useremo per calcolare l'area
+#scrivo la riga di stampa dove chiamo le variabili x ed y
+# il return mi serve per restituirmi le due variabili moltiplicate
+#   l'unico modo per "comunicare col mondo esterno"
+# posso assegnare un'altra variabile out che equivale a questa molt.
+#
+def area(x:float,y:float) -> float:
+    print (f' x={x}, y={y}')
+    #return x * y
+    out = x * y
+    print(f"l'area è {out}")
+
+""""
+print("bella")
+x,y = (2, 3)
+#print(area(x,y))
+out = area(x, y)
+print(out)
+"""
