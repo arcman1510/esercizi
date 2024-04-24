ea1d2ac7b893e68a1705eda20a35037cebf33eef
arcman1510 <arcman1510@gmail.com>
2024-04-24 18:47:25 +0200

    Tentativi di esercizi verifiche

--- verifiche.py
+++ verifiche.py
@@ -29,4 +29,59 @@ print(prime_factors(4))
 # Dato un elenco di valori delle carte che rappresentano 
 # una mano di blackjack, scrivi una funzione 
 # per determinare il valore totale della mano.
+""""
+def blackjack_hand_total(card: list[int]) -> int:
+    total = 0
+    aces = 0
+    for card in blackjack_hand_total:
+        card = str(card)
+        if card == "J" or card == "Q" or card== "K":
+            total+=10
+            continue
+        elif card == "2" or card == "3" or card == "4" or card == "5" or card == "6" or card == "7" or card == "8" or card == "9" or card == "10":
+            total += int(card)
+        elif card == "A":
+            total += 1
+            aces += 1
+    for _ in range(aces):
+        if total <= 11:
+            total += 10
+    return total
+    """
 
+#Q3
+# Scrivi una funzione che accetta una stringa come input, 
+# rimuove le parole non significative comuni stop_words 
+# e restituisce un dizionario in cui le chiavi sono 
+# parole univoche nel testo rimanente 
+# (ignorando la distinzione tra maiuscole e minuscole 
+# e la punteggiatura) e i valori sono le frequenze di quelle 
+# parole.
+#For example:
+
+# Test
+# stopwords = ['the', 'and', 'is', 'in', 'on', 'of']
+# text = "The quick brown fox jumps over the lazy dog. The dog is very lazy."
+# print(word_frequency(text, stopwords))
+# Result
+# {'quick': 1, 'brown': 1, 'fox': 1, 
+# 'jumps': 1, 'over': 1, 'lazy': 2, 'dog': 2, 'very': 1}
+"""
+# def word_frequency(text: str, stopwords: list[str]) -> dict[str, int]:
+
+    stopwords == [input()]
+    text == input()
+    for i in text:
+        x = text.split()
+        if i in x == stopwords:
+            text.remove(i)
+        else:
+            text.count(i)
+    """
+
+
+
+
+
+            
+        
