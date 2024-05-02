92d74a445c395dc6a85097c1422a6ee3f1eb29ce
arcman1510 <arcman1510@gmail.com>
2024-05-02 10:52:35 +0200

    Prova Q2

--- lezione5/lezione5esercizi.py
+++ lezione5/lezione5esercizi.py
@@ -13,10 +13,30 @@ def find_element(lst: list[int], element: int) -> bool:
             return True
         return False
 """   
+"""
 def find_element(lst: list[int], element: int) -> bool:
     for item in lst:
         if item == element:
             return True
         else:
             return False
-print(find_element)
+print(find_element([1, 2, 3, 4, 5], 5))
+"""
+
+"""
+Q2
+Scrivi una funzione che calcola la media 
+di una lista di numeri e ritorna il valore arrotondato all'intero più vicino.
+
+For example:
+Test 
+print(rounded_average([1, 1, 2, 2]))
+	
+Result
+2
+"""
+
+def rounded_average(numbers: list[int]) -> int:
+    for num in list:
+        return sum(list) / len(list)
+print(rounded_average([1, 1, 2, 2]))
