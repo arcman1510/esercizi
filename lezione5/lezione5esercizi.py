1813d03e1f756146fab0792419dfcc307cf7266a
arcman1510 <arcman1510@gmail.com>
2024-05-02 10:36:32 +0200

    Prova Q1

--- lezione5/lezione5esercizi.py
+++ lezione5/lezione5esercizi.py
@@ -4,9 +4,19 @@ La funzione dovrebbe determinare se un elemento è presente in una lista.
 Un errore nell'implementazione porta a risultati inaspettati.
  
 TROVA L'ERRORE E CORREGGI IL CODICE
-"""
+
+
+
 def find_element(lst: list[int], element: int) -> bool:
     for item in lst:
         if item == element:
             return True
         return False
+"""   
+def find_element(lst: list[int], element: int) -> bool:
+    for item in lst:
+        if item == element:
+            return True
+        else:
+            return False
+print(find_element)
